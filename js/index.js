0ba98135520465d6085d8308e42e213575cbfaeb
images <10404994+images332211@user.noreply.gitee.com>
2022-12-01 14:20:07 +0800

    tijiao

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 (function() {
-    console.log(111);
     $(".monitor .tabs").on("click", "a", function() {
         $(this)
             .addClass("active")
@@ -23,3 +22,53 @@
         $(this).append(rows);
     });
 })();
+//饼状图
+(function() {
+    var myChart = echarts.init(document.querySelector('.pie'));
+    option = {
+        tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        color: [
+            "#006cff",
+            "#60cda0",
+            "#ed8884",
+            "#ff9f7f",
+            "#0096ff",
+            "#9fe6b8",
+            "#32c5e9",
+            "#1d9dff"
+        ],
+        series: [{
+            name: '面积模式',
+            type: 'pie',
+            radius: ['10%', '65%'],
+            center: ['50%', '50%'],
+            roseType: 'radius',
+            data: [
+                { value: 20, name: "云南" },
+                { value: 26, name: "北京" },
+                { value: 24, name: "山东" },
+                { value: 25, name: "河北" },
+                { value: 20, name: "江苏" },
+                { value: 25, name: "浙江" },
+                { value: 30, name: "四川" },
+                { value: 42, name: "湖北" }
+            ],
+            // 修饰饼形图文字相关的样式 label对象
+            label: {
+                fontSize: 10
+            },
+            // 修饰引导线样式
+            labelLine: {
+                // 连接到图形的线长度
+                length: 6,
+                // 连接到文字的线长度
+                length2: 8
+            }
+        }],
+
+    };
+    myChart.setOption(option)
+})()
